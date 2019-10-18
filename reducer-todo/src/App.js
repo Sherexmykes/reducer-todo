b3aa9fd00121fdd2040b7e2fa4a4dd52c2abd830
Sheila R Moore <sherexmykes@github.com>
2019-10-17 22:22:09 -0400

    mvp completd

---
+++ reducer-todo/src/App.js
@@ -0,0 +1,17 @@
+import React, { useReducer } from 'react';
+import './App.css';
+import TodoList from './components/TodoList';
+import TodoForm from './components/TodoForm';
+import Todo from './components/Todo';
+
+import { reducer, todo } from './reducers/todoReducer';
+
+function App() {
+  return (
+    <div className="App">
+     
+      <TodoList />
+    </div>
+  );
+}
+export default App;
